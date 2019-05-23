fad0a8762df434c71e41b9825a679abc6bbac70c
RagvDest <rennygorozabel@hotmail.com>
2019-05-23 14:17:07 -0500

    Actividades

--- MyApplication2/app/src/main/java/com/example/myapplication/Actividad2.kt
+++ MyApplication2/app/src/main/java/com/example/myapplication/Actividad2.kt
@@ -3,6 +3,7 @@ package com.example.myapplication
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import kotlinx.android.synthetic.main.activity_actividad2.*
 
 class Actividad2 : AppCompatActivity() {
@@ -11,13 +12,19 @@ class Actividad2 : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_actividad2)
 
+        val nombre = intent.getStringExtra("nombre")
+        val edad = intent.getIntExtra("edad",0)
+        Log.i("intents","Nombre: $nombre")
+        Log.i("intents","Edad: $edad")
+
         btn_Actividad_1.setOnClickListener {
             irAActividadDos()
         }
     }
 
-    fun irAActividadDos(){
-        val intent = Intent(this,MainActivity::class.java)
+    fun irAActividadDos() {
+        val intent = Intent(this, MainActivity::class.java)
+        intent.flags = Intent.FLAG_ACTIVITY_NO_HISTORY
         startActivity(intent)
     }
 }
